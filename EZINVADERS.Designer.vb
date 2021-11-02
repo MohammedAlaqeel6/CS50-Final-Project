060e9a47c47791ff04690006a31ad7f02ffc04bc
MohammedAlaqeel6 <91496800+MohammedAlaqeel6@users.noreply.github.com>
2021-11-03 02:54:09 +0300

    Add files via upload

---
+++ EZINVADERS.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class EZINVADERS
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(EZINVADERS))
+        Me.Timermain = New System.Windows.Forms.Timer(Me.components)
+        Me.shot = New System.Windows.Forms.PictureBox()
+        Me.PNGshooter = New System.Windows.Forms.PictureBox()
+        CType(Me.shot, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PNGshooter, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timermain
+        '
+        Me.Timermain.Enabled = True
+        Me.Timermain.Interval = 1
+        '
+        'shot
+        '
+        Me.shot.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.shot.Location = New System.Drawing.Point(273, 450)
+        Me.shot.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.shot.Name = "shot"
+        Me.shot.Size = New System.Drawing.Size(12, 24)
+        Me.shot.TabIndex = 1
+        Me.shot.TabStop = False
+        '
+        'PNGshooter
+        '
+        Me.PNGshooter.BackColor = System.Drawing.SystemColors.Control
+        Me.PNGshooter.Image = Global.EZINVADERS.My.Resources.Resources.rocket__1_
+        Me.PNGshooter.InitialImage = CType(resources.GetObject("PNGshooter.InitialImage"), System.Drawing.Image)
+        Me.PNGshooter.Location = New System.Drawing.Point(273, 694)
+        Me.PNGshooter.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PNGshooter.Name = "PNGshooter"
+        Me.PNGshooter.Size = New System.Drawing.Size(130, 145)
+        Me.PNGshooter.TabIndex = 0
+        Me.PNGshooter.TabStop = False
+        '
+        'EZINVADERS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1036, 857)
+        Me.Controls.Add(Me.shot)
+        Me.Controls.Add(Me.PNGshooter)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.Name = "EZINVADERS"
+        Me.Text = "EZ INVADERS"
+        CType(Me.shot, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PNGshooter, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PNGshooter As PictureBox
+    Friend WithEvents Timermain As Timer
+    Friend WithEvents shot As PictureBox
+End Class
---
+++ EZINVADERS.vb
@@ -0,0 +1,192 @@
+﻿Public Class EZINVADERS
+    Dim SRight As Boolean
+    Dim SLeft As Boolean
+    Dim shooterspeed As Integer
+    Dim shotspeed As Integer
+    Dim invaderspeed As Integer
+    Dim invaderdrop As Integer
+    Const numofinvaders As Integer = 20
+    Dim iright(numofinvaders) As Boolean
+    Dim invader(numofinvaders) As PictureBox
+    Dim x As Integer
+    Dim shotdown As Integer
+    Dim paused As Boolean
+
+
+
+
+    Private Sub Timermain_Tick(sender As Object, e As EventArgs) Handles Timermain.Tick
+        moveshooter()
+        fireshot()
+        moveinvadr()
+        checkhit()
+        checkgameover()
+    End Sub
+
+
+    Private Sub EZINVADERS_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyValue = Keys.Right Then
+            SRight = True
+            SLeft = False
+        End If
+        If e.KeyValue = Keys.Left Then
+            SLeft = True
+            SRight = False
+        End If
+        If e.KeyValue = Keys.Space And shot.Visible = False Then
+            shot.Top = PNGshooter.Top
+            shot.Left = PNGshooter.Left + (PNGshooter.Width / 2) - (shot.Width / 2)
+            shot.Visible = True
+        End If
+
+    End Sub
+
+    Private Sub moveshooter()
+        If SRight = True And PNGshooter.Left + PNGshooter.Width < Me.ClientRectangle.Width Then
+
+            PNGshooter.Left += shooterspeed
+        End If
+        If SLeft = True And PNGshooter.Left > Me.ClientRectangle.Left Then
+            PNGshooter.Left -= shooterspeed
+        End If
+    End Sub
+
+    Private Sub EZINVADERS_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyValue = Keys.Right Then
+            SRight = False
+        End If
+        If e.KeyValue = Keys.Left Then
+            SLeft = False
+        End If
+    End Sub
+
+    Private Sub EZINVADERS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadinvaders()
+        loadsetting()
+    End Sub
+
+    Private Sub loadsetting()
+        paused = False
+        shotspeed = 20
+        shooterspeed = 8
+        shot.Visible = False
+        For Me.x = 1 To numofinvaders
+            iright(x) = True
+            invader(x).Left = (-50 * x) - (x * 5)
+            invader(x).Top = 0
+            invader(x).Visible = True
+        Next
+        invaderspeed = 3
+        invaderdrop = 50
+        shotdown = 0
+        SRight = False
+        SLeft = False
+        'My.Computer
+        Timermain.Enabled = True
+    End Sub
+
+    Private Sub fireshot()
+        If shot.Visible = True Then
+            shot.Top -= shotspeed
+        End If
+        If shot.Top + shot.Height < Me.ClientRectangle.Top Then
+            shot.Visible = False
+        End If
+    End Sub
+    Private Sub moveinvadr()
+        For Me.x = 1 To numofinvaders
+            If iright(x) = True Then
+                invader(x).Left += invaderspeed
+            Else
+                invader(x).Left -= invaderspeed
+
+            End If
+            If invader(x).Left + invader(x).Width > Me.ClientRectangle.Width And iright(x) = True Then
+                iright(x) = False
+                invader(x).Top += invaderdrop
+            End If
+            If invader(x).Left < Me.ClientRectangle.Left And iright(x) = False Then
+                iright(x) = True
+                invader(x).Top += invaderdrop
+            End If
+        Next
+    End Sub
+
+    Private Sub checkhit()
+        For Me.x = 1 To numofinvaders
+
+            If (shot.Top + shot.Height >= invader(x).Top) And
+                (shot.Top <= invader(x).Top + invader(x).Height) And
+                (shot.Left + shot.Width >= invader(x).Left) And
+                (shot.Left <= invader(x).Left + invader(x).Width) And
+                shot.Visible = True And
+                invader(x).Visible = True Then
+
+                invader(x).Visible = False
+                shotdown += 1
+                shot.Visible = False
+            End If
+        Next
+
+
+
+
+    End Sub
+
+    Private Sub checkgameover()
+        For Me.x = 1 To numofinvaders
+            If invader(x).Top + invader(x).Height >= PNGshooter.Top And invader(x).Visible = True Then
+                Timermain.Enabled = False
+                Me.x = numofinvaders
+                MsgBox("Game Over")
+                playagain()
+            End If
+
+
+        Next
+        If shotdown = numofinvaders Then
+            Timermain.Enabled = False
+            MsgBox("Earth Is Saved")
+            playagain()
+        End If
+    End Sub
+    Private Sub loadinvaders()
+        For Me.x = 1 To numofinvaders
+            invader(x) = New PictureBox
+            invader(x).Image = My.Resources.invaders
+            invader(x).Width = 60
+            invader(x).Height = 60
+            invader(x).BackColor = Color.Transparent
+            Controls.Add(invader(x))
+        Next
+    End Sub
+
+    Private Sub EZINVADERS_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+        If e.KeyChar = "p" Or e.KeyChar = "p" Then
+            If paused = True Then
+                Timermain.Enabled = True
+                paused = False
+            Else
+                Timermain.Enabled = False
+                paused = True
+            End If
+        End If
+    End Sub
+
+    Private Sub playagain()
+        Dim result = MsgBox("play again?", MsgBoxStyle.YesNo)
+        If result = MsgBoxResult.Yes Then
+            loadsetting()
+        Else
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub PNGshooter_Click(sender As Object, e As EventArgs) Handles PNGshooter.Click
+
+    End Sub
+
+    Private Sub shot_Click(sender As Object, e As EventArgs) Handles shot.Click
+
+    End Sub
+End Class
